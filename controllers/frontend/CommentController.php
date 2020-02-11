373a460f07352da665f5194ae05344bbde8c3423
Valentin Rocher <valentinrocher@gmail.com>
2020-02-11 10:19:32 +0100

    merge comments controllers in one

--- controllers/frontend/CommentController.php
+++ controllers/frontend/CommentController.php
@@ -3,29 +3,43 @@ require_once 'views/frontend/View.php';
 
 class CommentController
 {
-
     private $commentsManager;
     private $view;
 
     public function __construct()
     {
-        if (isset($url) && count($url) < 1) {
-            throw new \Exception('Page introuvable');
-        } else {
-            $this->newComment();
+        if ($_GET['action'] === 'add') {
+            $this->add($_GET['id']);
+        } else if ($_GET['action'] === 'report') {
+            $this->report($_GET['id'], $_GET['postId']);
         }
     }
 
-    private function newComment()
+    private function add($id)
     {
-        if (isset($_GET['id'])) {
+        if (isset($id)) {
             $this->commentsManager = new CommentsManager();
-            $affectedComment = $this->commentsManager->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+            $affectedComment = $this->commentsManager->addComment($id, $_POST['author'], $_POST['comment']);
 
-            if($affectedLines === false) {
+            if ($affectedLines === false) {
                 throw new Exception('Impossible d\'ajouter le commentaire !');
             } else {
-                header('Location: post&id=' . $_GET['id']);
+                header('Location: post&id=' . $id);
+                exit();
+            }
+        }
+    }
+
+    private function report($id, $postId)
+    {
+        if (isset($id) && isset($postId)) {
+            $this->commentsManager = new CommentsManager();
+            $affectedComment = $this->commentsManager->reportOneComment($id);
+
+            if ($affectedComment === false) {
+                throw new Exception('L\'action impossible');
+            } else {
+                header('Location: post&id=' . $postId);
                 exit();
             }
         }
