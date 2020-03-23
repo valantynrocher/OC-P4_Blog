ad0729090cfc0355d48da95f48a72d91194a980c
Valentin Rocher <valentinrocher@gmail.com>
2020-03-23 15:03:38 +0100

    update about view content

--- views/frontend/Static/about.php
+++ views/frontend/Static/about.php
@@ -20,33 +20,74 @@
 
 <section class="sample-text-area">
 	<div class="container">
-		<h3 class="text-heading">Text Sample</h3>
-		<p class="sample-text">
-			Every avid independent filmmaker has <b>Bold</b> about making that <i>Italic</i> interest documentary, or short
-			film to show off their creative prowess. Many have great ideas and want to “wow” the<sup>Superscript</sup> scene,
-			or video renters with their big project. But once you have the<sub>Subscript</sub> “in the can” (no easy feat), how
-			do you move from a <del>Strike</del> through of master DVDs with the <u>“Underline”</u> marked hand-written title
-			inside a secondhand CD case, to a pile of cardboard boxes full of shiny new, retail-ready DVDs, with UPC barcodes
-			and polywrap sitting on your doorstep? You need to create eye-popping artwork and have your project replicated.
-			Using a reputable full service DVD Replication company like PacificDisc, Inc. to partner with is certainly a
-			helpful option to ensure a professional end result, but to help with your DVD replication project, here are 4 easy
-			steps to follow for good DVD replication results:
 
-		</p>
+		<div>
+			<h3 class="text-heading">Ecrivain & aventurier</h3>
+			<p class="sample-text">
+			Jean Forteroche est un personnage inimitable et un écrivain
+			inclassable. Depuis 30 ans, ses aventures sur le globe lui ont inspiré
+			ses plus beaux livres et ses plus grands succès.
+			Pour son nouveau livre, Jean Forteroche perpétue son originalité
+			en vous proposant une lecture au fil de son écriture. De nos jours, nous
+			sommes sans cesse inviter à une lecture fractionnée, faite sur un écran
+			plutôt que sur un livre. C'est en souhaitant s'adapter à ce nouvel enjeux
+			que Jean Forteroche a souhaité proposer une lecture qui se veut progressive,
+			qui suivra ses périodes d'inspirations et ses périodes plus creuses.
+			</p>
+		</div>
+
 		<div class="section-top-border">
-			<h3 class="mb-30">Block Quotes</h3>
+			<h3 class="mb-30">Mon histoire</h3>
 			<div class="row">
-				<div class="col-lg-12">
-					<blockquote class="generic-blockquote">
-						“Recently, the US Federal government banned online casinos from operating in America by making it illegal to
-						transfer money to them through any US bank or payment system. As a result of this law, most of the popular
-						online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino
-						players found themselves being chased by the Federal government. But, after a fortnight, the online casino
-						industry came up with a solution and new online casinos started taking root. These began to operate under a
-						different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major
-						part of this was enlisting electronic banking systems that would accept this new clarification and start doing
-						business with me. Listed in this article are the electronic banking”
-					</blockquote>
+				<div class="col-md-4">
+					<div class="single-defination">
+						<h4 class="mb-20">16 mars 1969</h4>
+						<p>Jean naît à Nantes, une ville qu'il ne quittera quasiment jamais.</p>
+					</div>
+				</div>
+			
+				<div class="col-md-4">
+					<div class="single-defination">
+						<h4 class="mb-20">5 juillet 1987</h4>
+						<p>Jean obtient son baccalauréat Scientifique de justesse après des années scolaires difficiles où
+						Il ne trouve jamais de sens à ce qu'il fait. Pour lui, l'obtention du BAC sera un tremplin vers une
+						vie faite d'aventure.</p>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="single-defination">
+						<h4 class="mb-20">10 juillet 1987</h4>
+						<p>Quelques jours seulement après l'obtention du Bac, Jean part pour sa première aventure accompagné d'un ami.
+						Ensemble, ils se lancent le défi de rejoindre l'Italie avec seulement un sac à dos et quelques deniers en poche.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4">
+					<div class="single-defination">
+						<h4 class="mb-20">13 avril 1997</h4>
+						<p>Après plusieurs années faite de petits boulots et de voyages, Jean entreprend sa première aventure
+						d'exploration en solitaire sur les terres de l'Alaska. Ce sera le premier d'une longue série de voyage
+						sur ces terres isolées.</p>
+					</div>
+				</div>
+			
+				<div class="col-md-4">
+					<div class="single-defination">
+						<h4 class="mb-20">14 mai 1998</h4>
+						<p>Jean voit son livre "Loup solitaire" publié, une première pour lui. Ce premier roman marque le début
+						du succès.</p>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="single-defination">
+						<h4 class="mb-20">13 août 2009</h4>
+						<p>Jean se lance un nouveau défis avec l'exploration de de l'Asie pour la première fois et 
+						un nouveau roman comme objectif.</p>
+					</div>
 				</div>
 			</div>
 		</div>
